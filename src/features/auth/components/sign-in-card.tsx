c32f2aab502cf9e181ee22f1a3f5fb90125f22d7
XoDeR <xoder.vs@gmail.com>
2025-02-11 22:27:16 +0100

    sign in card

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,10 +1,12 @@
 import { DottedSeparator } from "@/components/dotted-separator"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle
 } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 
 export const SignInCard = () => {
   return (
@@ -14,9 +16,55 @@ export const SignInCard = () => {
           Welcome back!
         </CardTitle>
       </CardHeader>
-      <div className="px-7 mb-2">
+      <div className="px-7">
         <DottedSeparator />
       </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            required
+            type="email"
+            value={""}
+            onChange={() => { }}
+            placeholder="Enter email address"
+            disabled={false}
+          />
+          <Input
+            required
+            type="password"
+            value={""}
+            onChange={() => { }}
+            placeholder="Enter password"
+            disabled={false}
+            min={8}
+            max={256}
+          />
+          <Button disabled={false} size="lg" className="w-full">
+            Login
+          </Button>
+        </form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          Login with Google
+        </Button>
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          Login with Github
+        </Button>
+      </CardContent>
     </Card>
   )
 }
