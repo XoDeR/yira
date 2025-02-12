5051f17185947c33a888c2ff19b873f202fa875b
XoDeR <xoder.vs@gmail.com>
2025-02-12 22:54:27 +0100

    sign-in card validation with zod and using react-hook-form

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,9 @@
-const SignUpPage = () => {
-    return (
-        <div>Sign up</div>
+"use client"
 
-    );
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+const SignUpPage = () => {
+    return <SignUpCard />
 }
 
 export default SignUpPage;
