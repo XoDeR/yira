4c3eee1c0de0a91665b088baf2fc0c1975714e87
XoDeR <xoder.vs@gmail.com>
2025-02-15 00:21:06 +0100

    auth (login, register) and logout with appwrite using cookies and sessions

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "amstel-tech-yira-session";
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
+import { ID } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
+import { deleteCookie, setCookie } from "hono/cookie";
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post(
@@ -9,9 +13,18 @@ const app = new Hono()
     async (c) => {
       const { email, password } = c.req.valid("json");
 
-      console.log({ email, password });
+      const { account } = await createAdminClient();
+      const session = await account.createEmailPasswordSession(email, password);
 
-      return c.json({ email, password });
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "strict",
+        secure: true,
+        maxAge: 60 * 60 * 24 * 30,
+      });
+
+      return c.json({ success: true });
     })
   .post(
     "/register",
@@ -19,9 +32,26 @@ const app = new Hono()
     async (c) => {
       const { name, email, password } = c.req.valid("json");
 
-      console.log({ name, email, password });
+      const { account } = await createAdminClient();
+      await account.create(ID.unique(), email, password, name);
+      const session = await account.createEmailPasswordSession(email, password);
 
-      return c.json({ name, email, password });
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "strict",
+        secure: true,
+        maxAge: 60 * 60 * 24 * 30,
+      });
+
+      return c.json({ success: true });
+    })
+  .post(
+    "/logout",
+    (c) => {
+      deleteCookie(c, AUTH_COOKIE);
+
+      return c.json({ success: true });
     });
 
 export default app;
