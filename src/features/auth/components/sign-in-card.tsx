44fe31e93ba497f5b363c02f056bbf86852467b2
XoDeR <xoder.vs@gmail.com>
2025-02-14 20:59:52 +0100

    type safety for api with hono rpc (example with sign-in)

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -21,8 +21,11 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { loginSchema } from "../schemas";
+import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
+  const { mutate } = useLogin();
+
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -32,7 +35,7 @@ export const SignInCard = () => {
   });
 
   const onSubmit = (values: z.infer<typeof loginSchema>) => {
-    console.log({ values });
+    mutate(values);
   }
 
   return (
