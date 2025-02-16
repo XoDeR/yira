6f5434a96ff7fcf3124c53c181f47a32113dbd07
XoDeR <xoder.vs@gmail.com>
2025-02-16 22:26:51 +0100

    more elegant way to refresh the page (refreshes server side components)

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,10 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
 
 export const useLogout = () => {
+  const router = useRouter();
   const queryClient = useQueryClient();
 
   const mutation = useMutation<
@@ -16,7 +18,8 @@ export const useLogout = () => {
       return await response.json();
     },
     onSuccess: () => {
-      window.location.reload();
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["current"] });
     }
   });
 
