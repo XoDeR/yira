323a7a2c22c69cb573aa1d28f4bca014350f3c46
XoDeR <xoder.vs@gmail.com>
2025-02-17 21:41:21 +0100

    dashboard with sidebar layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,10 +6,16 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="min-h-screen">
       <div className="flex w-full h-full">
+        <div className="fixed left-0 top-0 hidden lg:block lg:w-[270px] h-full overflow-y-auto">
+          <Sidebar />
+        </div>
         <div className="lg:pl-[270px]">
-          {/* TODO: Navbar */}
-          <main></main>
-          {children}
+          <div className="mx-auto max-w-screen-2xl h-full">
+            {/* TODO: Navbar */}
+            <main className="h-full py-8 px-6 flex flex-col">
+              {children}
+            </main>
+          </div>
         </div>
       </div>
     </div>
