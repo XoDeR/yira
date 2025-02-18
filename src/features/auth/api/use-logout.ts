98810a2f91f2af60967cd3090c7ba7c688ad200f
XoDeR <xoder.vs@gmail.com>
2025-02-18 21:54:19 +0100

    added toaster from sonner for user notifications

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,7 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { InferRequestType, InferResponseType } from "hono";
+import { InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
 
@@ -15,11 +16,20 @@ export const useLogout = () => {
   >({
     mutationFn: async () => {
       const response = await client.api.auth.logout["$post"]();
+
+      if (!response.ok) {
+        throw new Error("Failed to log out");
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Logged out");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+    onError: () => {
+      toast.error("Failed to log out");
     }
   });
 
