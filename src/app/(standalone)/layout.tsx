571e33c77aad6feba720a0533347001d1b12be1b
XoDeR <xoder.vs@gmail.com>
2025-02-27 21:49:17 +0100

    fixed minor issues; added redirects if no user

--- src/app/(standalone)/layout.tsx
+++ src/app/(standalone)/layout.tsx
@@ -9,7 +9,7 @@ interface StandaloneLayoutProps {
 const StandaloneLayout = ({ children }: StandaloneLayoutProps) => {
   return (
     <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto mx-w-screen-2xl p-4">
+      <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center h-[80px]">
           <Link href="/">
             <Image src="/logo.svg" alt="logo" height={50} width={100} />
