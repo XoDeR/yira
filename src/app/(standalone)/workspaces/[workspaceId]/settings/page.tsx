087d96560dc3dfa0377805748f570e5e337b36da
XoDeR <xoder.vs@gmail.com>
2025-03-01 00:48:43 +0100

    edit workspace

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,4 +1,6 @@
 import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
 import { redirect } from "next/navigation";
 
 interface WorkspaceIdSettingsPageProps {
@@ -13,9 +15,15 @@ const WorkspaceIdSettingsPage = async ({
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+
+  if (!initialValues) {
+    redirect(`/workspaces/${params.workspaceId}`);
+  }
+
   return (
-    <div>
-      WorkspaceIdSettingsPage: {params.workspaceId}
+    <div className="w-full lg:max-w-xl">
+      <EditWorkspaceForm initialValues={initialValues} />
     </div>
   );
 }
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { SettingsIcon, UsersIcon } from "lucide-react";
-import { INSTRUMENTATION_HOOK_FILENAME } from "next/dist/lib/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GoHome, GoHomeFill, GoCheckCircle, GoCheckCircleFill } from "react-icons/go";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 const routes = [
   {
@@ -32,14 +35,18 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const workspaceId = useWorkspaceId();
+  const pathname = usePathname();
+
   return (
     <ul className="flex flex-col">
       {routes.map((item) => {
-        const isActive = false;
+        const fullHref = `/workspaces/${workspaceId}${item.href}`;
+        const isActive = pathname === fullHref;
         const Icon = isActive ? item.activeIcon : item.icon;
 
         return (
-          <Link key={item.href} href={item.href}>
+          <Link key={item.href} href={fullHref}>
             <div className={cn(
               "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
               isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
