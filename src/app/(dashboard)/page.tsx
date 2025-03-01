4f63c446aeaf5caa55adbc42c5b17bae2ea2b48b
XoDeR <xoder.vs@gmail.com>
2025-03-01 17:59:48 +0100

    correctly renamed auth/ actions to queries

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-import { getCurrent } from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 import { getWorkspaces } from "@/features/workspaces/actions";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 import { redirect } from "next/navigation";
