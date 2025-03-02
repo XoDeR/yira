1645020a2972d064c63f15a422b3371a8e09a992
XoDeR <xoder.vs@gmail.com>
2025-03-02 22:13:53 +0100

    members list

---
+++ src/features/workspaces/components/members-list.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { Fragment } from "react";
+import Link from "next/link";
+import { ArrowLeftIcon, MoreVerticalIcon } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Separator } from "@/components/ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+
+export const MembersList = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetMembers({ workspaceId });
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
+        <Button asChild variant="secondary" size="sm">
+          <Link href={`/workspaces/${workspaceId}`}>
+            <ArrowLeftIcon className="size-4 mr-2" />
+            Back
+          </Link>
+        </Button>
+        <CardTitle className="text-xl font-bold">
+          Members list
+        </CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        {data?.documents.map((member, index) => (
+          <Fragment key={member.$id}>
+            <div className="flex items-center gap-2">
+              <MemberAvatar
+                className="size-10"
+                fallbackClassName="text-lg"
+                name={member.name}
+              />
+              <div className="flex flex-col">
+                <p className="text-sm font-medium">{member.name}</p>
+                <p className="text-xs text-muted-foreground">{member.email}</p>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    className="ml-auto"
+                    variant="secondary"
+                    size="icon"
+                  >
+                    <MoreVerticalIcon className="size-4 text-muted-foreground" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="end">
+                  <DropdownMenuItem
+                    className="font-medium"
+                    onClick={() => { }}
+                    disabled={false}
+                  >
+                    Set as Administrator
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="font-medium"
+                    onClick={() => { }}
+                    disabled={false}
+                  >
+                    Set as Member
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="font-medium text-amber-700"
+                    onClick={() => { }}
+                    disabled={false}
+                  >
+                    Remove {member.name}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            {index < data.documents.length - 1 && (
+              <Separator className="my-2.5 bg-neutral-300" />
+            )}
+          </Fragment>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
