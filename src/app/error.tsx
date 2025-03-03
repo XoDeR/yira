03c358ecb19cb4d9c7e1a7e8f92f9bcc9a7381e1
XoDeR <xoder.vs@gmail.com>
2025-03-03 22:36:57 +0100

    updated error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,12 +6,12 @@ import { Button } from "@/components/ui/button";
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen flex flex-col gap-y-2 items-center justify-center">
-      <AlertTriangle />
-      <p className="text-sm text-muted-foreground">
+    <div className="h-screen flex flex-col gap-y-4 items-center justify-center">
+      <AlertTriangle className="size-6" />
+      <p className="text-sm">
         Something went wrong
       </p>
-      <Button variant="secondary">
+      <Button variant="secondary" size="sm">
         <Link href="/">
           Back to home
         </Link>
