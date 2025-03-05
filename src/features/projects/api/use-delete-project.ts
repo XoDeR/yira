545e18ddfee5b93723380e85a64ec60521752f96
XoDeR <xoder.vs@gmail.com>
2025-03-05 20:12:05 +0100

    delete project api

--- src/features/projects/api/use-delete-project.ts
+++ src/features/projects/api/use-delete-project.ts
@@ -4,10 +4,10 @@ import { client } from "@/lib/rpc";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
-type ResponseType = InferResponseType<typeof client.api.projects[":projectId"]["$patch"], 200>;
-type RequestType = InferRequestType<typeof client.api.projects[":projectId"]["$patch"]>;
+type ResponseType = InferResponseType<typeof client.api.projects[":projectId"]["$delete"], 200>;
+type RequestType = InferRequestType<typeof client.api.projects[":projectId"]["$delete"]>;
 
-export const useUpdateProject = () => {
+export const useDeleteProject = () => {
   const router = useRouter();
   const queryClient = useQueryClient();
   const mutation = useMutation<
@@ -15,23 +15,23 @@ export const useUpdateProject = () => {
     Error,
     RequestType
   >({
-    mutationFn: async ({ form, param }) => {
-      const response = await client.api.projects[":projectId"]["$patch"]({ form, param });
+    mutationFn: async ({ param }) => {
+      const response = await client.api.projects[":projectId"]["$delete"]({ param });
 
       if (!response.ok) {
-        throw new Error("Failed to update project");
+        throw new Error("Failed to delete project");
       }
 
       return await response.json();
     },
     onSuccess: ({ data }) => {
-      toast.success("Project updated");
+      toast.success("Project deleted");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["projects"] });
       queryClient.invalidateQueries({ queryKey: ["project", data.$id] });
     },
     onError: () => {
-      toast.error("Failed to update project");
+      toast.error("Failed to delete project");
     }
   });
 
