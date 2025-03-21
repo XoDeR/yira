927780b9eec3a0129efa2f7a28de41f5b7e2ed5a
XoDeR <xoder.vs@gmail.com>
2025-03-21 23:58:56 +0100

    update task (not finished yet)

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { useDeleteTask } from "../api/use-delete-task";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 interface TaskActionsProps {
   id: string;
@@ -18,6 +20,9 @@ interface TaskActionsProps {
 };
 
 export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
   const [ConfirmDialog, confirm] = useConfirm(
     "Delete task",
     "This action cannot be undone.",
@@ -30,7 +35,15 @@ export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
     if (!ok) return;
 
     mutate({ param: { taskId: id } });
-  }
+  };
+
+  const onOpenTask = () => {
+    router.push(`/workspaces/${workspaceId}/tasks/${id}`);
+  };
+
+  const onOpenProject = () => {
+    router.push(`/workspaces/${workspaceId}/projects/${projectId}`);
+  };
 
   return (
     <div className="flex justify-end">
@@ -41,14 +54,14 @@ export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-48">
           <DropdownMenuItem
-            onClick={() => { }}
+            onClick={onOpenTask}
             className="font-medium p-[10px]"
           >
             <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
             Task Details
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => { }}
+            onClick={onOpenProject}
             className="font-medium p-[10px]"
           >
             <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
