e8e72e3deb07c26aa2a00e57e07c45294a525cbf
XoDeR <xoder.vs@gmail.com>
2025-03-29 19:56:35 +0100

    kanban cards with drag-n-drop functionality

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -9,6 +9,7 @@ import {
 
 import { Task, TaskStatus } from "../types";
 import { KanbanColumnHeader } from "./kanban-column-header";
+import { KanbanCard } from "./kanban-card";
 
 const boards: TaskStatus[] = [
   TaskStatus.BACKLOG,
@@ -59,6 +60,33 @@ export const DataKanban = ({
                 board={board}
                 taskCount={tasks[board].length}
               />
+              <Droppable droppableId={board}>
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="min-h-[200px] py-1.5"
+                  >
+                    {tasks[board].map((task, index) => (
+                      <Draggable
+                        key={task.$id}
+                        draggableId={task.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <KanbanCard task={task} />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  </div>
+                )}
+              </Droppable>
             </div>
           )
         })}
