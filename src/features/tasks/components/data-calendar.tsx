3fddb924922394f628582c5fcd6fc67fec608a2d
XoDeR <xoder.vs@gmail.com>
2025-04-10 22:47:48 +0200

    finished calendar page

--- src/features/tasks/components/data-calendar.tsx
+++ src/features/tasks/components/data-calendar.tsx
@@ -8,11 +8,13 @@ import {
 } from "date-fns";
 import { enUS } from "date-fns/locale";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { Task } from "../types";
 import { useState } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./data-calendar.css";
 import { EventCard } from "./event-card";
+import { Button } from "@/components/ui/button";
 
 const locales = {
   "en-US": enUS
@@ -29,6 +31,36 @@ const localizer = dateFnsLocalizer({
 interface DataCalendarProps {
   data: Task[];
 };
+
+interface CustomToolbarProps {
+  date: Date;
+  onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+};
+
+const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
+  return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+      <Button
+        onClick={() => onNavigate("PREV")}
+        variant="secondary"
+        size="icon"
+      >
+        <ChevronLeftIcon className="size-4" />
+      </Button>
+      <div className="flex itens-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+        <CalendarIcon className="size-4 mr-2" />
+        <p className="text-sm">{format(date, "MMMM yyyy")}</p>
+      </div>
+      <Button
+        onClick={() => onNavigate("NEXT")}
+        variant="secondary"
+        size="icon"
+      >
+        <ChevronRightIcon className="size-4" />
+      </Button>
+    </div>
+  )
+}
 
 export const DataCalendar = ({
   data,
@@ -80,6 +112,9 @@ export const DataCalendar = ({
             project={event.project}
             status={event.status}
           />
+        ),
+        toolbar: () => (
+          <CustomToolbar date={value} onNavigate={handleNavigate} />
         )
       }}
     />
