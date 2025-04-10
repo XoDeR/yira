1d04996f078b261b3829e7d43e1ca13e966a3321
XoDeR <xoder.vs@gmail.com>
2025-04-10 22:10:15 +0200

    event card updated

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -2,6 +2,9 @@ import { Project } from "@/features/projects/types";
 import { TaskStatus } from "../types";
 import { cn } from "@/lib/utils";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 interface EventCardProps {
   title: string;
@@ -26,9 +29,19 @@ export const EventCard = ({
   status,
   id,
 }: EventCardProps) => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
+  const onClick = (
+    e: React.MouseEvent<HTMLDivElement>
+  ) => {
+    e.stopPropagation();
+    router.push(`/workspaces/${workspaceId}/tasks/${id}`)
+  };
+
   return (
     <div className="px-2">
-      <div className={cn(
+      <div onClick={onClick} className={cn(
         "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-1.5 cursor-pointer hover:opacity-75 transition",
         statusColorMap[status]
       )}>
@@ -38,6 +51,10 @@ export const EventCard = ({
             name={assignee?.name}
           />
           <div className="size-1 rounded-full bg-neutral-300" />
+          <ProjectAvatar
+            name={project?.name}
+            image={project?.imageUrl}
+          />
         </div>
       </div>
     </div>
