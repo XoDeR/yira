e25361f9b7baf746c42336c1ab6492964257316c
XoDeR <xoder.vs@gmail.com>
2025-05-03 14:18:23 +0200

    fix name

--- src/features/tasks/components/task-description.tsx
+++ src/features/tasks/components/task-description.tsx
@@ -28,7 +28,7 @@ export const TaskDescription = ({ task }: TaskDescriptionProps) => {
   return (
     <div className="p-4 border rounded-lg">
       <div className="flex items-center justify-between">
-        <p className="text-lg font-semibold">Overview</p>
+        <p className="text-lg font-semibold">Description</p>
         <Button onClick={() => setIsEditing((prev) => !prev)} size="sm" variant="secondary">
           {isEditing ? (
             <XIcon className="size-4 mr-2" />
