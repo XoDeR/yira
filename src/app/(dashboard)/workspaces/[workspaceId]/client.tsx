285d1331a6004fc91a66b6cf676702c9f6a3ff86
XoDeR <xoder.vs@gmail.com>
2025-05-20 22:05:24 +0200

    task list component

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { Analytics } from "@/components/analytics";
+import { DottedSeparator } from "@/components/dotted-separator";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
@@ -11,6 +14,9 @@ import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal
 import { Task } from "@/features/tasks/types";
 import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { CalendarIcon, PlusIcon, SettingsIcon } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+import Link from "next/link"
 
 export const WorkspaceIdClient = () => {
   const workspaceId = useWorkspaceId();
@@ -21,7 +27,6 @@ export const WorkspaceIdClient = () => {
   const { data: members, isLoading: isLoadingMembers } = useGetMembers({ workspaceId });
 
   const { open: createProject } = useCreateProjectModal();
-  const { open: createTask } = useCreateTaskModal();
 
   const isLoading =
     isLoadingAnalytics ||
@@ -41,20 +46,67 @@ export const WorkspaceIdClient = () => {
     <div className="h-full flex flex-col space-y-4">
       <Analytics data={analytics} />
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-        <TaskList />
+        <TaskList data={tasks.documents} total={tasks.total} />
       </div>
     </div>
   );
 };
 
 interface TaskListProps {
-  tasks: Task[];
+  data: Task[];
+  total: number;
 }
 
-export const TaskList = ({ tasks }: TaskListProps) => {
+export const TaskList = ({ data, total }: TaskListProps) => {
+  const workspaceId = useWorkspaceId();
+  const { open: createTask } = useCreateTaskModal();
+
   return (
     <div className="flex flex-col gap-y-4 col-span-1">
-      Tasks
+      <div className="bg-muted rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">
+            Tasks ({total})
+          </p>
+          <Button variant="muted" size="icon" onClick={createTask}>
+            <PlusIcon className="size-4 text-neutral-400" />
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <ul className="flex flex-col gap-y-4">
+          {data.map((task) => (
+            <li key={task.$id}>
+              <Link href={`/workspaces/${workspaceId}/tasks/${task.$id}`}>
+                <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                  <CardContent className="p-4">
+                    <p className="text-lg font-medium truncate">
+                      {task.name}
+                    </p>
+                    <div className="flex items-center gap-x-2">
+                      <p>{task.project?.name}</p>
+                      <div className="size-1 rounded-full bg-neutral-300" />
+                      <div className="text-sm text-muted-foreground flex items-center">
+                        <CalendarIcon className="size-3 mr-1" />
+                        <span className="truncate">
+                          {formatDistanceToNow(new Date(task.dueDate))}
+                        </span>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No tasks found
+          </li>
+        </ul>
+        <Button variant="muted" className="mt-4 w-full" asChild>
+          <Link href={`/workspaces/${workspaceId}/tasks`}>
+            Show All
+          </Link>
+        </Button>
+      </div>
     </div>
   )
 }
